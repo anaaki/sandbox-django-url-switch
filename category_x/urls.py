10c68e759db12207bcb2a2190c37daa39ff0883d
= <=>
2019-01-03 02:19:33 +0900

    set back to index link using url func

--- category_x/urls.py
+++ category_x/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'category_x'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<template_name>', views.static_render, name=''),
