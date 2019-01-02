3ecb0a863a4ff3c4ac4f19a455942ed19116cc20
= <=>
2019-01-03 01:48:18 +0900

    add minimum app and templates settings

--- category_x/views.py
+++ category_x/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {"yes": 0}
+    return render(request, "category_x/index.html", context)
--- samplesite/settings.py
+++ samplesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'category_x.apps.CategoryXConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
