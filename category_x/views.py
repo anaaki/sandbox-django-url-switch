7c8b76e15b6e8d0afa5ba89f0d812623bd7bbd03
= <=>
2019-01-03 02:14:40 +0900

    use static path

--- category_x/views.py
+++ category_x/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    context = {"yes": 0}
+    context = {
+        "link_items":("aa","bb")
+    }
     return render(request, "category_x/index.html", context)
+
+
+def static_render(request, template_name):
+    context = {"template_name": template_name}
+    return render(request,
+                  "category_x/{}.html".format(template_name),
+                  context)
