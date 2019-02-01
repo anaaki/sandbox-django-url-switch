da1319c89878243b73d6fe3574ef5bd0b46586b7
= <=>
2019-02-02 00:51:03 +0900

    version1系の書き方

--- category_x/urls.py
+++ category_x/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'category_x'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<template_name>', views.static_render, name='static_render'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<template_name>.+)/$', views.static_render, name='static_render'),
 ]
